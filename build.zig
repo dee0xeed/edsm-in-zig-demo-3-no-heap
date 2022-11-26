b379cc76359979127bc198b98ddce976418d0b6d
zed <dee0xeed@gmail.com>
2022-11-26 18:33:42 +0300

    new file:   LICENSE.txt new file:   build.zig

---
+++ build.zig
@@ -0,0 +1,13 @@
+
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable("dirmon", "src/main.zig");
+    exe.single_threaded = true;
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.install();
+}
